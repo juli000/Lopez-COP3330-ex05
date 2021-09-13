7d8f321de2bba1f54a0061cd854b876f23405045
Julio Lopez <julioandreslpz@gmail.com>
2021-09-12 22:17:26 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Julio Lopez
+ */
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "What is the first number? " );
+        Scanner s = new Scanner(System.in);
+        String fir = s.nextLine();
+        System.out.println("What is the second number?");
+        String sec = s.nextLine();
+        Integer i = Integer.valueOf(fir);
+        Integer j = Integer.valueOf(sec);
+        System.out.println(fir + " + " + sec + " = " + (i + j) + "\n" + fir + " - " + sec + " = " + (i - j)+ "\n" + fir + " * " + sec + " = " + (i * j) + "\n" + fir + " / " + sec + " = " + (i / j));
+    }
+}
